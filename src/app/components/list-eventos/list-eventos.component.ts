72841791819d6acac32eab5749112bd50f213c30
Dani Martinez <daniel.martinez-perez@capgemini.com>
2024-10-25 09:44:19 +0200

    Estructura principal y componente listar

---
+++ src/app/components/list-eventos/list-eventos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-eventos',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-eventos.component.html',
+  styleUrl: './list-eventos.component.scss'
+})
+export class ListEventosComponent {
+
+}
