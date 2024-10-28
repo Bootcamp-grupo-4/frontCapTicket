3268683aafff1204c73c078ae38aa87a989621e0
Dani Martinez <daniel.martinez-perez@capgemini.com>
2024-10-28 12:12:35 +0100

    Error view & redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { ListEventosComponent } from './components/list-eventos/list-eventos.component';
 import { AddEventoComponent } from './components/add-evento/add-evento.component';
 import { EditEventosComponent } from './components/edit-eventos/edit-eventos.component';
+import { ErrorViewComponent } from './components/error-view/error-view.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/list', pathMatch: 'full' },
     { path: 'list', component: ListEventosComponent },
     { path: 'add', component: AddEventoComponent },
-    { path: 'edit/:id', component: EditEventosComponent}
+    { path: 'edit/:id', component: EditEventosComponent},
+    { path: '**', component: ErrorViewComponent }
 ];
